f8715470b3938dc53b89756ed80542d62ea456da
Austin Jones <austin.jones231@gmail.com>
2018-03-05 01:37:42 -0500

    Added user/pass and Bearer token authentication

--- api_keys_default.py
+++ api_keys_default.py
@@ -1,5 +1,12 @@
-API_KEYS = {
+EXCHANGE = {
     'apiKey': 'API_PUBLIC_KEY',
     'secret': 'API_PRIVATE_KEY',
     'passphrase': 'API_PASSPHRASE'
+}
+
+SERVICES = {
+    'metis': {
+        'user_id': 'METIS_USER_ID',
+        'password': 'METIS_PASSWORD'
+    }
 }
